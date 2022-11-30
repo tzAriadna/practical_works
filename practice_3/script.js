7a849f8f7983bef3ec22a455cfea178e72169c80
Ariadna Tuzina <68476340+tzAriadna@users.noreply.github.com>
2022-11-30 20:11:15 +0700

    исправила. Избавилась от конструктора

--- practice_3/script.js
+++ practice_3/script.js
@@ -1,85 +1,70 @@
 class Operation {
-	constructor(a, b) {
-		this.a = a;
-		this.b = b;
-	}
 	getName() {}
 	getSign() {}
+	estimates(a, b) {}
 }
 
 class DIV extends Operation {
-	constructor(a, b) {
-		super(a, b);
-	}
-	get result() {
-		return (this.a - this.a % this.b) / this.b;
-	}
 	getName() {
 		return 'Целая часть от деления'
 	}
 	getSign() {
 		return ' div '
 	}
+	estimates(a, b) {
+		return (a - a % b) / b;
+	}
 }
 
 class MOD extends Operation {
-	constructor(a, b) {
-		super(a, b);
-	}
-	get result() {
-		return this.a % this.b;
-	}
 	getName() {
 		return 'Отсаток от деления'
 	}
 	getSign() {
 		return ' mod '
 	}
+	estimates(a, b) {
+		return a % b;
+	}
 }
 
 class NOD extends Operation {
-	constructor(a, b) {
-		super(a, b);
-	}
-	get result() {
-		while (this.a != this.b){
-			if (this.a > this.b){
-				this.a -= this.b;
-			} else {
-				this.b -= this.a;
-			}
-		}
-	return (this.a);
-	}
 	getName() {
 		return 'Наименьший общий делитель'
 	}
 	getSign() {
 		return ' nod '
 	}
+	estimates(a, b) {
+		while (a != b){
+			if (a > b){
+				a -= b;
+			} else {
+				b -= a;
+			}
+		}
+		return a;
+	}
 }
 
 class NOK extends Operation {
-	constructor(a, b) {
-		super(a, b);
-	}
-	get result() {
-		var new_a = this.a;
-		var new_b = this.b
-		while (this.a != this.b){
-			if (this.a > this.b){
-				this.a -= this.b;
-			} else {
-				this.b -= this.a;
-			}
-		}
-		return (Math.abs(new_a * new_b)/this.a);
-	}
 	getName() {
 		return 'Наименьшее общее кратное'
 	}
 	getSign() {
 		return ' nok '
+	}
+	estimates(a, b) {
+		var new_a = a;
+		var new_b = b
+		while (a != b){
+			if (a > b){
+				a -= b;
+			} else {
+				b -= a;
+			}
+		}
+		return (Math.abs(new_a * new_b)/a);
 	}
 }
 
@@ -88,14 +73,14 @@ var a = 215;
 var b = 33;
 
 let operations = [
-	new DIV(a, b),
-	new MOD(a, b),
-	new NOD(a, b),
-	new NOK(a, b),
+	new DIV(),
+	new MOD(),
+	new NOD(),
+	new NOK(),
 ];
 
 for (let op of operations) {
 	console.log(op.getName());
-	console.log(a + op.getSign() + b + '=' + op.result);
+	console.log(a + op.getSign() + b + '=' + op.estimates(a, b));
 }
 
